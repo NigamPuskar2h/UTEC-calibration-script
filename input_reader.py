345f8f6eaf31fb578dee16d636a8e70e0369164b
Nigam <nigam.puskar@2hoffshore.com>
2026-06-02 16:07:19 +0100

    Added user input for column heading titles

--- input_reader.py
+++ input_reader.py
@@ -7,6 +7,15 @@ def read_input_yaml(input_yaml_file):
 
     with open(input_yaml_file, "r") as f:
         InputYaml = yaml.load(f)
+    class DataInputs:
+        def __init__(self, data):
+            self.original_logger_headings = data["logger_headings"] 
+            
+            self.remove_logger_headings = {
+            k: v for k, v in self.original_logger_headings.items()
+            if v == "n/a"
+        }
+
 
     class FilePaths:
         def __init__(self, path):
@@ -14,6 +23,7 @@ def read_input_yaml(input_yaml_file):
 
     class InputConfig:
         def __init__(self, input_yaml):
+            self.data = DataInputs(input_yaml["Data Inputs"])
             self.path = FilePaths(input_yaml["File Paths"])
 
     config = InputConfig(InputYaml)
@@ -23,26 +33,23 @@ def read_csv(file):
     df = pd.read_csv(file, header=None, names=["raw"])
     return df
 
-def parse_logger(df):
+def parse_logger(df, config):
     logger_df = df.iloc[:, 0].str.extractall(r'Result:\s*(?P<Result>[\d.E+-]+).*?').astype(float)
     logger_df = logger_df.unstack()
-
-    #These names are not altered to reflect the actual values from the logger
-    column_names = ({
-        0: "ArX",
-        1: "ArY", 
-        2: "n/a", 
-        3: "AccX", 
-        4: "n/a", 
-        5: "AccZ", 
-        6: "AccY", 
-        7: "AccT"
-        })
+    logger_df.columns = logger_df.columns.get_level_values(1)
     
+    column_names = config.data.original_logger_headings
     logger_df = logger_df.rename(columns = column_names)
+
     return logger_df
+
+def format_logger_df(df, config):
+    cols_to_remove = list(config.data.remove_logger_headings.values())
+    format_logger_df = df.drop(columns = cols_to_remove)
+    format_logger_df = format_logger_df.sort_index(axis=1)
+    return format_logger_df
 
 def clean_df(df):
     df = df.dropna(axis=0, how='any')
     return df
-    
+
--- logic.py
+++ logic.py
@@ -5,8 +5,10 @@ def main_logic():
     config = input_reader.read_input_yaml(r"Input.yaml")
     file = config.path.logger
     df = input_reader.read_csv(file)
-    logger_df = input_reader.parse_logger(df)
+    logger_df = input_reader.parse_logger(df, config)
     logger_df = input_reader.clean_df(logger_df)
-    average_df = calculation.df_average(logger_df)
+    format_logger_df = input_reader.format_logger_df(logger_df, config)
+    #average_df = calculation.df_average(logger_df)
     #SD_df = calculation.df_SD(logger_df)
-    print(average_df)
+    #print(average_df)
+    print(format_logger_df)
