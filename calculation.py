350dce32dc2961c565b01140edbf8a8a9e72691f
Nigam <nigam.puskar@2hoffshore.com>
2026-06-03 16:48:23 +0100

    Created expected acc function

--- calculation.py
+++ calculation.py
@@ -15,3 +15,36 @@ def df_SD(df):
     SD_df = df.std()
     SD_df = SD_df.to_frame().T
     return(SD_df)
+
+def expected_acc_values(df_logger_avg):
+    df_logger_avg_acc = df_logger_avg.drop(["ArX", "ArY"], axis=1)
+    df_expected_acc = pd.DataFrame(index=df_logger_avg_acc.index, columns = df_logger_avg_acc.columns)
+    
+    downwards_volts = 3
+    orthogonal_volts = 2
+    upwards_volts = 1
+
+    downwards_g = 1
+    orthogonal_g = 0
+    upwards_g = -1
+
+    buffer = 0.2
+    
+    for i in range(df_logger_avg_acc.shape[0]):
+        for j in range(df_logger_avg_acc.shape[1]):
+            if df_logger_avg_acc.iloc[i,j] >= downwards_volts - buffer and df_logger_avg_acc.iloc[i,j] <= downwards_volts + buffer:
+                df_expected_acc.iloc[i,j] = downwards_g    
+
+            elif df_logger_avg_acc.iloc[i,j] >= orthogonal_volts - buffer and df_logger_avg_acc.iloc[i,j] <= orthogonal_volts + buffer:
+                df_expected_acc.iloc[i,j] = orthogonal_g    
+
+            elif df_logger_avg_acc.iloc[i,j] >= upwards_volts - buffer and df_logger_avg_acc.iloc[i,j] <= upwards_volts + buffer:
+                df_expected_acc.iloc[i,j] = upwards_g
+  
+            else:
+                print("Out of range")
+
+    return(df_expected_acc)
+
+
+
--- logic.py
+++ logic.py
@@ -6,23 +6,21 @@ def main_logic():
     config = input_reader.read_input_yaml(r"Input.yaml")
     file = config.path.logger
     sheet_index = [2,3,4,5,6,7,8,9]
-    df_dict_original = input_reader.read_xlsx(file, sheet_index)
-    #df = input_reader.clean_df(df)
-
-
-    results = []
-    df_dict_cleaned = {}
-    keys = list(df_dict_original.keys())
+    df_logger_original = input_reader.read_xlsx(file, sheet_index)
+    
+    logger_avg = []
+    df_logger_cleaned = {}
+    keys = list(df_logger_original.keys())
 
     for i in range(len((sheet_index))):
         key = keys[i]
-        values = df_dict_original[key]
+        values = df_logger_original[key]
 
-        df_dict_cleaned[key] = input_reader.clean_df(values)
+        df_logger_cleaned[key] = input_reader.clean_df(values)
+        logger_avg.append(calculation.df_average(values))
 
-        results.append(calculation.df_average(values))
+    df_logger_avg = pd.concat(logger_avg)
 
-    res_df = pd.concat(results)
-    print(df_dict_original)
-    print(df_dict_cleaned)
-    print(res_df)
+    df_expected_acc = calculation.expected_acc_values(df_logger_avg)
+    print(df_logger_avg)
+    print(df_expected_acc)    
